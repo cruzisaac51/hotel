24375b8974d2d8246103d9322c690d8c41c752fb
Isaac Cruz <76910938+cruzisaac51@users.noreply.github.com>
2023-11-07 19:14:05 -0600

    getting ready to connect to fl0

--- hotel/settings.py
+++ hotel/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3id!&b7^yhc0^c8x#z@0&q&2z=dkpgb&t80(#q0pqotpqcoe5l'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
